7b11843dfaa0ac08e49d62e87721e341f428d7f9
Mathew Lee <mwlee9@gmail.com>
2025-03-18 16:34:18 -0500

    Initial Commit

---
+++ src/lib/Navbar.svelte
@@ -0,0 +1,28 @@
+<script>
+    import './navbar.css';
+</script>
+
+
+<div class="navbar bg-neutral text-neutral-content">
+    <div class="flex-1">
+      <a class="btn btn-ghost text-xl">daisyUI</a>
+    </div>
+    <div class="flex-none">
+      <ul class="menu menu-horizontal px-1">
+        <li><a>Link</a></li>
+        <li>
+          <details>
+            <summary>Parent</summary>
+            <ul class="bg-base-100 rounded-t-none p-2">
+                <a href="#Home">Home</a>
+                <a href="#About">About</a>
+                <a href="#Experience">Experience</a>
+                <a href="#Projects">Projects</a>
+                <a href="#Connect">Connect</a>
+            </ul>
+          </details>
+        </li>
+      </ul>
+    </div>
+  </div>
+
---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+    import "../app.css";
+    import Navbar from '$lib/Navbar.svelte';
+</script>
+
+<Navbar />
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<button class="btn w-64 rounded-full">Button</button>
+<button class="btn btn-primary">Button</button>
